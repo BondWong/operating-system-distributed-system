a8a86586bd0befdfec67031b8b2733b82a7cc5f7
Bond Wong <bond@Bonds-MacBook-Pro.local>
2019-11-02 16:06:55 -0700

    .

--- src/store.cc
+++ src/store.cc
@@ -84,8 +84,7 @@ private:
 				std::vector<std::thread> threads_;
 				for (auto ip_addr: ip_addresses) {
 					VendorClient vc = VendorClient(grpc::CreateChannel(ip_addr, grpc::InsecureChannelCredentials()));
-					store::ProductReply vendor_reply_;
-					threads_.push_back(std::thread(&VendorClient::AsyncCompleteRpc, &vc, std::ref(vendor_reply_)));
+					threads_.push_back(std::thread(&VendorClient::AsyncCompleteRpc, &vc, std::ref(reply_)));
 					vc.getProductBid(request_.product_name());
 				}
 				for (std::vector<std::thread>::iterator it = threads_.begin() ; it != threads_.end(); ++it) it->join();
